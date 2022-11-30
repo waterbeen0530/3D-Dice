654283cda27bc0945a09b0c6889bce85a260589a
임수빈 <hange253@gmail.com>
2022-11-30 09:26:44 +0900

    [CREATE] rock

---
+++ src/Pages/Rock.jsx
@@ -0,0 +1,11 @@
+import Dice from "../components/rock/Dice";
+import Title from "../components/rock/Title";
+
+export default function App() {
+  return (
+    <>
+      <Title />
+      <Dice />
+    </>
+  );
+}
