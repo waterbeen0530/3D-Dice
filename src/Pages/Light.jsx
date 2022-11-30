070cde1c6da9e3af39324fb2928e39b26ed81cc4
임수빈 <hange253@gmail.com>
2022-11-30 09:27:09 +0900

    [CREATE] light

---
+++ src/Pages/Light.jsx
@@ -0,0 +1,11 @@
+import Dice from "../components/light/Dice";
+import Title from "../components/light/Title";
+
+export default function App() {
+  return (
+    <>
+      <Title />
+      <Dice />
+    </>
+  );
+}
---
+++ src/components/light/Title.jsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export default function Title() {
+  return (
+    <Container>
+      <Text>
+        <h1>번개 원소와 공명하기</h1>
+        <Row></Row>
+      </Text>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  width: 100vw;
+  text-align: center;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const Text = styled.div`
+  position: relative;
+  margin: 50px 0 0 0;
+  h1 {
+    margin: 0;
+    line-height: 70px;
+    font-size: 50px;
+  }
+`;
+
+const Row = styled.div`
+  position: absolute;
+  width: 100px;
+  height: 20px;
+  background-color: fff;
+`;
